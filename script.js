ff36388e9e5deba296b13b5b5adc05a2d6e1d847
Nazim Bayraktar <nab1079@hotmail.com>
2022-02-11 21:26:14 +0000

    create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+// const prices = [];
+
+
+
+
+
+
+
+// function totalAmount() {
+//   let sum = 0;
+//   for (i = 0; i < servicePrice.length; i++) {
+
+//     sum += servicePrice[i];
+
+
+
+//   }
+//   console.log(sum);
+// }
+// totalAmount()
