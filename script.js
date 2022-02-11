8a678e644032237850efb5d38c97c9d839eecf97
Nazim Bayraktar <nab1079@hotmail.com>
2022-02-11 23:41:00 +0000

    midified idex.html, style.css, script.js

--- script.js
+++ script.js
@@ -2,22 +2,35 @@ const prices = [];
 const btnWash = document.getElementById('btn-wash');
 const btnMow = document.getElementById('btn-mow');
 const btnPull = document.getElementById('btn-pull');
-
+const washCarP = document.getElementById('wash-car-p');
+const mowLownP = document.getElementById('mow-lawn-p');
+const pullWeedsP = document.getElementById('pull-weeds-p');
+const totalAmount = document.getElementById('total-amount');
 
 btnWash.addEventListener('click', function () {
-  addPrice(10);
+  addPriceToArray(10, washCarP);
+
 })
 
 btnMow.addEventListener('click', function () {
-  addPrice(20);
+  addPriceToArray(20, mowLownP);
 })
 
 btnPull.addEventListener('click', function () {
-  addPrice(30);
+  addPriceToArray(30, pullWeedsP);
 })
 
-
-function addPrice(amount) {
+function addPriceToArray(amount, services) {
   prices.push(amount);
-  console.log(prices);
+  services.innerHTML += amount;
+  calculateTotal()
+}
+
+
+function calculateTotal() {
+  let sum = 0;
+  for (let i = 0; i < prices.length; i++) {
+    sum += prices[i];
+  }
+  return totalAmount.innerHTML += sum;
 }
