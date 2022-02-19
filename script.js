8f4d71eade5ee9811411e6969990508edb9a485f
Nazim Bayraktar <nab1079@hotmail.com>
2022-02-19 01:50:40 +0000

    still not fully working

--- script.js
+++ script.js
@@ -5,11 +5,12 @@ const btnPull = document.getElementById('btn-pull');
 const washCarP = document.getElementById('wash-car-p');
 const mowLownP = document.getElementById('mow-lawn-p');
 const pullWeedsP = document.getElementById('pull-weeds-p');
+const spanEl = document.getElementById('span');
 const totalAmount = document.getElementById('total-amount');
+
 
 btnWash.addEventListener('click', function () {
   addPriceToArray(10, washCarP);
-
 })
 
 btnMow.addEventListener('click', function () {
@@ -21,16 +22,19 @@ btnPull.addEventListener('click', function () {
 })
 
 function addPriceToArray(amount, services) {
-  prices.push(amount);
-  services.innerHTML += amount;
-  calculateTotal()
+  if (washCarP.innerHTML === '0' || mowLownP.innerHTML === '0' || pullWeedsP.innerHTML === '0') {
+    let taskSum = 0;
+    spanEl.innerHTML = `${taskSum += 1}`;
+    prices.push(amount);
+    services.innerHTML = `£${amount}`;
+    calculateTotal()
+  }
 }
-
 
 function calculateTotal() {
   let sum = 0;
   for (let i = 0; i < prices.length; i++) {
     sum += prices[i];
   }
-  return totalAmount.innerHTML += sum;
+  return totalAmount.innerHTML = `£${sum}`;
 }
